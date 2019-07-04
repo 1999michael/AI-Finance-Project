d90285cea677154805285ff75e240a605e192ebd
kailin hong <kailin.hong@mail.utoronto.ca>
2019-07-04 01:21:58 -0400

    getSymbol

--- main.py
+++ main.py
@@ -9,16 +9,5 @@ def main():
     a = getStockDataValue.getStockSymbol()
     getStockDataValue.convertSymToJsonFile(a)
 
-# if name == "main":
-#  slideManager = SlideManager()
-#  slideManager.baseDirectory = "/Users/kailin/Desktop/base_dir"
-
-#  slideManager.addSlide(1)
-#  slide1 = slideManager.getSlide(1)
-
-#  image = Image("/Users/kailin/Desktop/tmp/1006/IAPS_N_7043.jpg")
-#  slide1.addImage(image)
-
-
 if __name__ == "__main__":
     main()
