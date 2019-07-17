5d425e06a9815d28f185f9bc32aca5fd6a3dd5a1
kailin hong <kailin.hong@mail.utoronto.ca>
2019-07-17 18:08:46 -0400

    minor changes to previous branch

--- download.py
+++ download.py
@@ -6,7 +6,6 @@ def download():
     getStockDataValue = getStockData()
     #getStockDataValue.convertSymToJsonFile()
     getStockDataValue.downloadDataToLocal()
-    #getStockDataValue.getstockprice('2016-11-18', 'KL')
     getStockDataValue.MultiCompanyPrice()
 if __name__ == "__main__":
     download()
