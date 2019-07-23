1098bd8e03a49ec1cbefec8eaecac497cb111bed
Michael Ruan <michael.ruan@mail.utoronto.ca>
2019-07-23 10:40:54 -0400

    Compiled dictionary values to list

--- complete_data_to_list.py
+++ complete_data_to_list.py
@@ -1,7 +1,8 @@
 import json
 import ast
+import datetime
 
-def read_from_database():                       # Take in raw .txt file, and convert to dictionary
+def read_from_database():                                       # Take in raw str, and convert to dictionary
         with open("data_list_complete.json") as f:
                 content = f.readlines()
         content = [x.strip() for x in content] 
@@ -10,43 +11,60 @@ def read_from_database():                       # Take in raw .txt file, and con
                 data.append(ast.literal_eval(content[i]))       # Converting to dictionary
         return data
 
-data = read_from_database()[0]
-data_dict = data
-company_names = data["2019-06-06"]
-company_names = company_names.keys()
-print(company_names)
-#print(data)
-company_names_test = data["2016-01-06"]
-company_names_test = company_names_test.keys()
-print(company_names_test)
+def sorting(L):
+        splitup = L.split('-')
+        return splitup[0], splitup[1], splitup[2]
 
-data = data.keys()
-data.sort() # Days 
+data = read_from_database()[0]                  # Get Database Data
 
-num_days = 891
-num_companies = 119 # any day works
-num_data_points = 8 # any company works
+list_companies = data["2019-07-17"].keys()              # Company names
+list_companies.sort(reverse = False)                    # Sort alphabetically company names
 
-print(data)
+list_days = data.keys()                                 # Get Database days
+list_days = sorted(list_days, key = sorting)            # Sort days in past -> future order
+        
+bad_companies = ["WLKP", "VMC", "VRS", "REX"]
+bad_days = []
+
+for day in list_days:
+        num_companies = len(data[day])
+        for company in list_companies:
+                if (company in bad_companies and company in data[day]):
+                        del data[day][company]
+                if (num_companies < 161):
+                        del data[day]
+                        bad_days.append(day)
+                        break
+
+print("Num days: ", len(data))
+
+for company in bad_companies:
+        list_companies.remove(company)
+
+for day in bad_days:
+        list_days.remove(day)
+
+# Give AGX data for April 14th (accidentally omitted)
+data["2016-04-14"]["AGX"] = data["2016-04-15"]["AGX"]
+
+
+num_days = len(data)
+num_companies = len(data["2016-04-14"])
+num_data_points = len(data["2016-04-14"]["AGX"])
+
 
 list_data = [[[0 for i in range(num_data_points)] for j in range(num_companies)] for k in range(num_days)]
-
-print(len(list_data))
-print(len(list_data[0]))
-print(len(list_data[0][0]))
 
 counter_i = 0
 counter_j = 0
 counter_k = 0
-for day in data:
-        for company in company_names:
+for day in list_days:
+        for company in list_companies:
                 for data_point in range(0,8):
-                        list_data[counter_i][counter_j][counter_k] = data_dict[day][company][data_point]
+                        list_data[counter_i][counter_j][counter_k] = data[day][company][data_point]
                         counter_k += 1
                 counter_k = 0
                 counter_j += 1
         counter_j = 0
         counter_i += 1
 
-print(list_data)
-#print(list_data)
