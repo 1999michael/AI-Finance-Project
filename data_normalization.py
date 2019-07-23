10016632989b129e3d4d80257ccbcb3cdf0b9799
Michael Ruan <michael.ruan@mail.utoronto.ca>
2019-07-23 10:51:58 -0400

    integrate comlete data to list with data normalizatiom file

--- data_normalization.py
+++ data_normalization.py
@@ -8,6 +8,7 @@ import numpy as np
 import random
 import math
 import json
+import ast
 
 # ===========================================================
 # ================= Random input generator ==================
@@ -27,6 +28,78 @@ def random_input_gen(num_data_per_day = 9, num_companies = 165, num_days = 763):
     data[0][0][0] = 15
     data[0][0][1] = 0.05
     return data
+
+# ============================================================
+# ===== Formatting data from .json to dictionary to list =====
+# ============================================================
+
+def read_from_database_to_list():                               # Take in raw str, and convert to dictionary, then to list
+        with open("data_list_complete.json") as f:
+                content = f.readlines()
+        content = [x.strip() for x in content] 
+        data = []
+        for i in range(0,len(content)):
+                data.append(ast.literal_eval(content[i]))       # Converting to dictionary
+
+        data = data[0]
+
+        list_companies = data["2019-07-17"].keys()              # Company names
+        list_companies.sort(reverse = False)                    # Sort alphabetically company names
+
+        list_days = data.keys()                                 # Get Database days
+        list_days = sorted(list_days, key = sorting)            # Sort days in past -> future order
+                
+        bad_companies = ["WLKP", "VMC", "VRS", "REX"]           # These companies data are incomplete. Remove (not enough data)
+        bad_days = []
+
+        for day in list_days:
+                num_companies = len(data[day])
+                for company in list_companies:
+                        if (company in bad_companies and company in data[day]):
+                                del data[day][company]
+                        if (num_companies < 161):
+                                del data[day]
+                                bad_days.append(day)
+                                break
+
+        print("Num days: ", len(data))
+
+        for company in bad_companies:                           # Remove bad companies from company list
+                list_companies.remove(company)
+
+        for day in bad_days:                                    # Remove bad days from days list
+                list_days.remove(day)
+
+        # Give AGX data for April 14th (accidentally omitted)
+        data["2016-04-14"]["AGX"] = data["2016-04-15"]["AGX"]
+
+
+        num_days = len(data)                                    # Dimensions of data
+        num_companies = len(data["2016-04-14"])
+        num_data_points = len(data["2016-04-14"]["AGX"])
+
+
+        list_data = [[[0 for i in range(num_data_points)] for j in range(num_companies)] for k in range(num_days)]
+
+        counter_i = 0                                           # Input all data to premade 3D list in order
+        counter_j = 0
+        counter_k = 0
+        for day in list_days:
+                for company in list_companies:
+                        for data_point in range(0,8):
+                                list_data[counter_i][counter_j][counter_k] = data[day][company][data_point]
+                                counter_k += 1
+                        counter_k = 0
+                        counter_j += 1
+                counter_j = 0
+                counter_i += 1
+        return list_data
+
+def sorting(L):
+        splitup = L.split('-')
+        return splitup[0], splitup[1], splitup[2]
+
+# FINAL LIST IS THE VARIABLE: list_data
 
 # ===========================================================
 # ====================== Normalization ======================
@@ -389,8 +462,7 @@ def format_data(data, normalize_function = 2, normalize_company = 1, num_range =
         num_days = 763
         data = random_input_gen(num_data_per_day, num_companies, num_days)
     elif (data == True):
-        with open("data_list_complete.json","r") as json_file:
-            data = json.load(json_file)
+        data = read_from_database_to_list()
     
     # Get length of data input
     num_data_per_day = len(data[0][0])
